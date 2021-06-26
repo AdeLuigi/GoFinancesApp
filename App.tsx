480862ab214c88986f7263dc61331ddc20f4d6e2
Ademario Vitor <ademario63@gmail.com>
2021-06-26 18:59:14 -0300

    Categories

--- App.tsx
+++ App.tsx
@@ -9,7 +9,8 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme'
-import Dashboard from "./src/screens/Register";
+import Register from "./src/screens/Register";
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -24,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard/>
+      <CategorySelect/>
     </ThemeProvider>
   );
 }
